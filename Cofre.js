e086f0c14622c6436e7e29c7c5e5a5ce927590b1
Glaucyrlei P da Silva <ganestgamer11@gmail.com>
2022-02-11 15:08:11 -0300

    Arquivos do cofre

---
+++ Cofre.js
@@ -0,0 +1,61 @@
+function validar() {
+			var conteudo = document.getElementById('cofre').value;
+
+			if((conteudo.length < 8) || (conteudo.length > 14)){
+				alert("Tamanho de senha invalido")
+			}else{
+
+				var letra = /[a-z]{1}/i;
+
+				var conteudoCLetra = letra.exec(conteudo[0]);
+
+				if (conteudoCLetra) {
+					var simbolos = /[#@!$]/g;
+					var conteudoCSimbolo = simbolos.exec(conteudo);
+					var auxi = "";
+
+					while(conteudoCSimbolo){
+						var n = conteudoCSimbolo.index;
+						auxi += conteudo[n];
+						conteudoCSimbolo = simbolos.exec(conteudo);
+					}
+					
+					if (auxi.length>=2) {
+						var letrasMaiusculas = /[A-Z]/;
+						var letrasMinusculas = /[a-z]/;
+						var conteudoCMaiusculas = letrasMaiusculas.exec(conteudo);
+						var contedoCMinusculas = letrasMinusculas.exec(conteudo);
+						
+						if (conteudoCMaiusculas && contedoCMinusculas) {
+							var num = /[0-9]/g;
+							var conteudoCNumeros = num.exec(conteudo);
+							var auxin = "";
+
+							while(conteudoCNumeros){
+								var z = conteudoCNumeros.index;
+								auxin += conteudo[z];
+								conteudoCNumeros = num.exec(conteudo);
+							}
+
+							if (auxin.length>=2) {
+								alert("Senha valida");
+							}else{
+								alert("Senha deve conter dois numeros");
+							}
+
+
+						}else{
+							alert("Senha deve conter pelo menos um algarismo mauisculo");
+						}
+
+
+					}else{
+						alert("Senha contem menos de dois simbolos");
+					}
+
+				}else{
+					alert("primeira algarismo deve ser uma letra")
+				}
+			}
+			
+		}
